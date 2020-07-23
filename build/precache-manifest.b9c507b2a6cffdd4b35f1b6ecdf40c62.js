f3345df18db6f0cd350c78880dfc3812a9d51048
Linda Woo <lindawoo@Lindas-MacBook-Pro.local>
2020-07-22 21:21:50 -0700

    background photos added

---
+++ build/precache-manifest.b9c507b2a6cffdd4b35f1b6ecdf40c62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879891853f05081acaac96fe7bd570b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a3f8cb0fab54ed400d1",
+    "url": "/static/css/main.12aa0a76.chunk.css"
+  },
+  {
+    "revision": "09f2acf42dc0c1bfb650",
+    "url": "/static/js/2.e22da2c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e22da2c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3f8cb0fab54ed400d1",
+    "url": "/static/js/main.39db5596.chunk.js"
+  },
+  {
+    "revision": "d09278ab860102e2fdd1",
+    "url": "/static/js/runtime-main.1e5982b8.js"
+  },
+  {
+    "revision": "a9d832d2271c596a4ce2fa096f0ae81f",
+    "url": "/static/media/prstore.a9d832d2.jpg"
+  },
+  {
+    "revision": "392dd3d944d1d9706d085eadacb2be6c",
+    "url": "/static/media/seattle6.392dd3d9.jpg"
+  }
+]);
--- src/Landing.js
+++ src/Landing.js
@@ -1,11 +1,13 @@
 import React, {Component} from "react";
 import classes from "./Landing.module.css";
+import Prstore from "./prstore.jpg"
 
 class Landing extends Component {
     state={
         landingRef : React.createRef(),
         phinneyRef : React.createRef(),
-        comeRef: React.createRef()
+        comeRef: React.createRef(),
+        storeRef:React.createRef()
     }
 
     render(){
@@ -13,11 +15,19 @@ class Landing extends Component {
         let place = 0;
         window.addEventListener("scroll", (e)=>{
             if(this.state.landingRef.current){
-            place = window.pageYOffset;
-            this.state.landingRef.current.style.top = `${-place}px`;
-            if(place<170){
-            this.state.phinneyRef.current.style.transform = `translate(0,${3*place}px)`;
-            }
+                place = window.pageYOffset;
+                this.state.landingRef.current.style.top = `${-place}px`;
+                if(place<170){
+                    this.state.phinneyRef.current.style.transform = `translate(0,${3*place}px)`;
+                    }
+                // if(place<450){    
+                // this.state.comeRef.current.style.transform = `translate(0,${-.5*place}px)`;
+                // // this.state.storeRef.current.style.transform = `translate(0,${-.5*place}px)`;
+                // }else{
+                //     this.state.comeRef.current.style.transform = `translate(0,${place}px)`;
+                // }
+                console.log(place)
+           
             }
         })
         
@@ -30,6 +40,9 @@ class Landing extends Component {
                 <div className = {classes.PhotoContainer}>
                 </div>
                 <p className = {classes.Come} ref = {this.state.comeRef}>Come Enjoy the View</p>
+                <div className = {classes.StorefrontContainer}>
+                    <img src = {Prstore} ref = {this.state.storeRef}/>
+                </div>
             </div>
             
             
